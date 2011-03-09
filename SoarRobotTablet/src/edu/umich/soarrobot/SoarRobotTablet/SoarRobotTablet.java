e4b92f3be13ac422c02917639ea732fc87aaa7ec
Kevin <kevin@eecs-laird1.(none)>
2011-03-09 11:18:55 -0500

    Changed ip address to Kevin's machine

--- SoarRobotTablet/src/edu/umich/soarrobot/SoarRobotTablet/SoarRobotTablet.java
+++ SoarRobotTablet/src/edu/umich/soarrobot/SoarRobotTablet/SoarRobotTablet.java
@@ -139,7 +139,8 @@ public class SoarRobotTablet extends Activity
             setSelectedObject(null);
             try
             {
-                robotSession = new RobotSession(this, "141.212.109.194", 12122);
+                //robotSession = new RobotSession(this, "141.212.109.194", 12122); // Miller's ip
+                robotSession = new RobotSession(this, "141.212.109.194", 12122); // Kevin's ip
                 robotSession.start();
             }
             catch (Exception e)
