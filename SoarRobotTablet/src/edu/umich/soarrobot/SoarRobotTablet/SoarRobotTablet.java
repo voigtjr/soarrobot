888ee3560f0776b7bb894a312a916edd077f6c7c
Kevin <kevin@eecs-laird1.(none)>
2011-03-09 11:02:18 -0500

    Minor changes

--- SoarRobotTablet/src/edu/umich/soarrobot/SoarRobotTablet/SoarRobotTablet.java
+++ SoarRobotTablet/src/edu/umich/soarrobot/SoarRobotTablet/SoarRobotTablet.java
@@ -139,7 +139,7 @@ public class SoarRobotTablet extends Activity
             setSelectedObject(null);
             try
             {
-                robotSession = new RobotSession(this, "141.212.109.139", 12122);
+                robotSession = new RobotSession(this, "141.212.109.194", 12122);
                 robotSession.start();
             }
             catch (Exception e)
